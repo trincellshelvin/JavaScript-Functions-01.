cec588d5b3180b00949944c30687e4e82065caa5
trincellshelvin <trincellshelvin@gmail.com>
2024-07-31 03:31:01 +0000

    day 1 completed song with extracted repeats

--- index.js
+++ index.js
@@ -1,1 +1,84 @@
+function refrain() {
+    console.log("For Your glory");
+    console.log("I will do anything");
+    console.log("Just to see You");
+    console.log("To behold You as my King");
+}
 
+function refrain2() {
+    console.log("Lord if I Find favor in Your sight");
+    console.log("Lord, please hear my hearts cry");
+}
+
+function refrain3() {
+    console.log("I'm desperately waiting to be where You are");
+    console.log("I'll 'cross the hottest dessert");
+    console.log("I'll travel near or far");
+}
+
+function refrain4() {
+    console.log("Lord if I Find favor in Your sight");
+    console.log("Lord please, Lord please (Lord Please)");
+    console.log("Hear my hearts cry (hear my hearts cry)");
+}
+
+function refrain5() {
+    console.log("I'm desperately waiting (I'm desperately waiting)");
+    console.log("To be where You are (to be where You are");
+    console.log("I'll 'cross the hottest dessert (I'll 'cross the hottest dessert)");
+    console.log("I'll travel near or far");
+}
+
+function refrain6() {
+    console.log("For Your glory (for your glory)");
+    console.log("I will do anything(I will do anything)");
+    console.log("Just to see You (Just to see You)");
+    console.log("To behold You as my King (to behold you as my king)");
+}
+
+function refrain7() {
+    console.log("I wanna be where you are (I really wanna be there)");
+    console.log("I gotta be where you are");
+    console.log("I wanna be where you are");
+    console.log("I gotta be where you are");
+}
+
+function refrain8() {
+    console.log("I wanna be where you are");
+    console.log("Gotta be where you are (I wanna be there)");
+    console.log("I wanna be where you are");
+    console.log("Gotta be where you are");
+}
+
+function refrain9() {
+    console.log("I wanna be where you are");
+    console.log("Peace is where you are");
+    console.log("Joy is where you are");
+    console.log("And Love is who you are");
+}
+
+refrain2();
+
+refrain3();
+
+refrain();
+
+refrain();
+
+refrain4();
+
+refrain5();
+
+refrain6();
+
+refrain6();
+
+refrain6();
+
+refrain7();
+
+refrain8();
+
+refrain();
+
+refrain9();
