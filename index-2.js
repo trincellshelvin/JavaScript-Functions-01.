a567c5d1f1240446330a7f3661537bf2005717f1
trincellshelvin <trincellshelvin@gmail.com>
2024-08-02 14:55:27 +0000

    completed song js 2

---
+++ index-2.js
@@ -0,0 +1,23 @@
+function refrain(){
+    let words = "";
+    words += "For Your glory"
+    words += "I will do anything"
+    words += "Just to see You"
+    words += "To behold You as my King"
+    return words;
+}
+
+function song(){
+    let r;
+    song +="Lord if I Find favor in Your sight"
+    song +="Lord, please hear my hearts cry"
+    
+    r = refrain
+    console.log(r)
+
+    song += "I'm desperately waiting to be where You are"
+    song += "I'll 'cross the hottest dessert"
+    song += "I'll travel near or far"
+    return song;
+}
+
